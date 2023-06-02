c98c14badb2f50b6a71a7eab51382b4c06db714b
snehawadekar <wadekarsneha15@gmail.com>
2023-06-02 12:00:03 +0530

    UN2

--- from_clause.py
+++ from_clause.py
@@ -52,7 +52,7 @@ def getCoreRelations(method = 'rname'):
 	if reveal_globals.global_db_engine != 'Microsoft SQL Server':
 		try:
 			cur = reveal_globals.global_conn.cursor()
-			cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' and TABLE_CATALOG='tpch_oj';")
+			cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' and TABLE_CATALOG='tpch10';")
 			# cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' and TABLE_CATALOG='" + reveal_globals.global_db_instance + "';")
 			res = cur.fetchall()			
 			print(res)
